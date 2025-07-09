48ea3353cac25ef0336b9db5d70b1bf081693c16
Jalen DeLoney <jalendeloney@gmail.com>
2025-07-09 19:25:04 +0000

    Set up Auto-deployment with Pythonanywhere.

--- web_page.py
+++ web_page.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', subtitle='Home Page', text='This is the home page')
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/jalenseotechdev/flask-practice')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
