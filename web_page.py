d7397ef857a5d991beda990f609bf8ccb342fb6e
Jalen DeLoney <jalendeloney@gmail.com>
2025-07-09 19:56:20 +0000

    Added basic form and styling.

--- web_page.py
+++ web_page.py
@@ -1,11 +1,29 @@
 import git
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
 from flask import Flask, render_template, url_for, flash, redirect, request
+
 app = Flask(__name__)
+proxied = FlaskBehindProxy(app)
+
+app.config['SECRET_KEY'] = '1cd4b57b7f34608f90863ed47d72b353'
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', subtitle='Home Page', text='This is the home page')
+    
+@app.route("/second_page")
+def second_page():
+    return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
 
 @app.route("/update_server", methods=['POST'])
 def webhook():
